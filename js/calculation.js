3cb1d4cd1a06d1f8e07f6080d24f614eaeb7ba4b
Mahbubgit <bitu_rajshahi@yahoo.com>
2022-02-17 19:57:32 +0600

    save calculation

--- js/calculation.js
+++ js/calculation.js
@@ -4,6 +4,39 @@ document.getElementById('calculate-button').addEventListener('click', function (
     calculate();
 });
 
+//  For Saving Button Click Event 
+
+document.getElementById('save-button').addEventListener('click', function () {
+    calculate();
+
+    const savePercentageInput = document.getElementById('save-percentage');
+    const savingAmountText = document.getElementById('saving-amount');
+    const remainingBalanceText = document.getElementById('remaining-balance');
+
+    const savePercentageText = savePercentageInput.innerText;
+    const savePercentageValue = parseFloat(savePercentageText);
+    const savePercentageAmount = (incomeAmount * savePercentageValue) / 100;
+
+    // error-saving-message for wrong type of number
+    // invalid-saving-message for abnormal income and savings
+
+    const errorSavingMessage = document.getElementById('error-saving-message');
+    const invalidSavingMessage = document.getElementById('invalid-saving-message');
+
+    if (typeof savePercentageValue != 'number' || savePercentageValue < 0 || isNaN(incomeAmount)) {
+        invalidSavingMessage.style.display = 'none';
+        errorSavingMessage.style.display = 'block';
+    }
+    else {
+        invalidSavingMessage.style.display = 'none';
+        errorSavingMessage.style.display = 'none';
+        //--------------------------Pending
+        savingAmountText.innerText = savePercentageAmount;
+        remainingBalanceText.innerText = balanceAmount - savePercentageAmount;
+
+    }
+});
+
 // calculate function
 
 function calculate() {
@@ -45,3 +78,4 @@ function calculate() {
         balance.innerText = balanceAmount;
     }
 }
+
